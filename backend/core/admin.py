9a08c34579ff5a9674e42b8f07123b868a182026
Jaci-Rojas <jaci.rojas15@myhunter.cuny.edu>
2026-03-10 19:15:13 -0400

    Registered the models in admin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile, Song, Vocabulary, UserProgress
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Song)
+admin.site.register(Vocabulary)
+admin.site.register(UserProgress)
