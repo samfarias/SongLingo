d98d4c435649853e7b878e08d4a10c8779ea15cf
Austin Robertson <162934591+arobertson7@users.noreply.github.com>
2026-03-10 21:54:14 -0400

    added comment in admin.py regarding admin site

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# example below (this allows us to see our model data in the /admin page):
+# from .models import UserProfile
+# admin.site.register(UserProfile)
