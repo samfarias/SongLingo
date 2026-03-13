cb596d0eacaf63f2471c131ec88d32e7ee18204a
Derek Huang <derekhuang9@gmail.com>
2026-03-13 14:10:33 -0400

    Added Color

--- ios/SongLingo/SongLingo/Login.swift
+++ ios/SongLingo/SongLingo/Login.swift
@@ -14,13 +14,25 @@ struct Login: View {
     var body: some View {
         ZStack {
             
+            LinearGradient(
+                colors: [
+                    Color(red: 0.984, green: 0.443, blue: 0.522),
+                    Color(red: 0.576, green: 0.200, blue: 0.918),
+                    Color(red: 0.231, green: 0.027, blue: 0.392)],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
             VStack(spacing: 20) {
                 
                 Text("SongLingo")
                     .font(.largeTitle)
                     .fontWeight(.bold)
+                    .foregroundColor(.white)
                 
                 Text("Learn languages through the music you love")
+                    .foregroundColor(.white)
                     
                     Text("Welcome Back")
                         .font(.title2)
@@ -36,7 +48,7 @@ struct Login: View {
                     
                     TextField("", text: $email)
                         .padding()
-                        .background(Color.gray.opacity(0.2))
+                        .background(.gray.opacity(0.2))
                         .cornerRadius(10)
                     
                     Text("Password")
@@ -44,7 +56,7 @@ struct Login: View {
                     
                     SecureField("", text: $password)
                         .padding()
-                        .background(Color.gray.opacity(0.2))
+                        .background(.gray.opacity(0.2))
                         .cornerRadius(10)
                     
                     Text("Don't have an account?")
