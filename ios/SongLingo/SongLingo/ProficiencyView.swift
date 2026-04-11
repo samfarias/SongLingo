036439872f1d656a04307d62a1b7697284db55e1
Derek Huang <derekhuang9@gmail.com>
2026-04-11 22:37:09 +0800

    Create Proficiency Screen

---
+++ ios/SongLingo/SongLingo/ProficiencyView.swift
@@ -0,0 +1,18 @@
+//
+//  ProficiencyView.swift
+//  SongLingo
+//
+//  Created by Derek Huang on 4/11/26.
+//
+
+import SwiftUI
+
+struct ProficiencyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProficiencyView()
+}
