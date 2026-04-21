a4cbee32ef88b056b1820eb402be702c87f9d49b
Derek Huang <derekhuang9@gmail.com>
2026-04-21 00:36:52 -0400

    Create GenreSelectView

---
+++ ios/SongLingo/SongLingo/GenreSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  GenreSelectView.swift
+//  SongLingo
+//
+//  Created by Derek Huang on 4/21/26.
+//
+
+import SwiftUI
+
+struct GenreSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenreSelectView()
+}
