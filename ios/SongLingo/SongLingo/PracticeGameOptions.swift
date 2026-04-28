24a539ea4253c4e157aacc48d309b207c8f0f564
Jaci-Rojas <jaci.rojas15@myhunter.cuny.edu>
2026-04-28 05:49:25 -0400

    Game Options Screen

--- ios/SongLingo/SongLingo/PracticeGameOptions.swift
+++ ios/SongLingo/SongLingo/PracticeGameOptions.swift
@@ -9,7 +9,61 @@ import SwiftUI
 
 struct PracticeGameOptions: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    //Sends to Word Cards Screen
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.purple.opacity(0.5))
+                        VStack (alignment: .leading) {
+                            Text("Word Cards")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                                .font(.title2)
+                        }
+                        .padding(.horizontal)
+                        .padding(.vertical)
+                    }
+                    .padding()
+                    .padding(.bottom, 30)
+                    
+                    //Sends to Lyric Match Screen
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.purple.opacity(0.5))
+                        VStack (alignment: .leading) {
+                            Text("Lyric Match")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                                .font(.title2)
+                        }
+                        .padding(.horizontal)
+                        .padding(.vertical)
+                    }
+                    .padding()
+                    .padding(.bottom, 30)
+                    
+                    //Sends to Complete the Lyrics Screen
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.purple.opacity(0.5))
+                        VStack (alignment: .leading) {
+                            Text("Complete the Lyrics")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                                .font(.title2)
+                        }
+                        .padding(.horizontal)
+                        .padding(.vertical)
+                    }
+                    .padding()
+                }
+                .padding(.top, 100)
+            }
+            .navigationTitle("Practice New Words!")
+            .background(Color.purple.opacity(0.2))
+        }
     }
 }
 
