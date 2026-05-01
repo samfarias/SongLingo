78de1dcabbfc1b2d874a1c63aba83f90b969466d
Derek Huang <derekhuang9@gmail.com>
2026-05-01 10:28:48 -0400

    Add enum-based navigation flow

---
+++ ios/SongLingo/SongLingo/OnboardingFlow.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingFlow.swift
+//  SongLingo
+//
+//  Created by Derek Huang on 4/28/26.
+//
+
+import SwiftUI
+
+enum OnboardingFlow : Hashable {
+    case createAccView
+    case languageSelectionView
+    case proficiencyView
+    case genreSelectView
+    case dashboard
+}
