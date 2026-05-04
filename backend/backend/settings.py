551d1679f4cde340343d966aed33b66791cca1ee
samfarias <samfarias8@gmail.com>
2026-05-04 02:47:08 +0000

    Security leak: .env pushed, keys swapped & gitignore added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['69.183.31.175', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['69.183.31.175', '68.183.31.175:8000', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS =['*']
 
 # Application definition
 
